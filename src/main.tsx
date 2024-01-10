f2c22fb0f1e45d3a007cb365d453f1d525c1c868
Hrynchuk Taras <taras.hrynchuk.ri.2023@lpnu.ua>
2024-01-10 17:23:10 +0200

    build

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
-      <BrowserRouter basename="/react-registration">
+      <BrowserRouter>
          <AuthProvider>
             <Routes>
                <Route path="/*" element={<App />}></Route>
